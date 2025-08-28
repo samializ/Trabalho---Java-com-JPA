b4e2ebac8351c73819fdea1c03245a310930e37d
samializ <160865650+samializ@users.noreply.github.com>
2025-08-28 17:39:23 -0300

    Add files via upload

---
+++ JPAUtil.java
@@ -0,0 +1,15 @@
+package util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("meuPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
